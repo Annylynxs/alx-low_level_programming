0a43c9c17f3bfa13242e29c343bb82ab35366d47
Annylynxs <annakintola7@gmail.com>
2023-04-21 16:15:17 -0700

    reverse

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - printing hexadecimal
+ *Return: (0)
+ */
+int main(void)
+{
+	int n;
+	char p;
+
+	n = 0;
+	p = 97;
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n==;
+	}
+	while (p <=102)
+	{
+		putchar(p);
+		p++
+	}
+	putchar('\n');
+	return (0);
+}
