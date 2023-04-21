9383bcf710f29b52a3a14a9e8c7a0153702295ba
Annylynxs <annakintola7@gmail.com>
2023-04-21 15:21:38 -0700

    uppercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing lowercase
+ * Return: (0)
+ */
+int main(void)
+{
+	char n;
+
+	n = 97;
+	while (n <= 122)
+	{
+		if (n != 113 && n != 101)
+		{
+			putchar(n);
+			n++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
