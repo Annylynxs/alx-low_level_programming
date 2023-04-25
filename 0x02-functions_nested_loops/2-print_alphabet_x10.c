19cac2da6c92f23533e517b3615a2b3eb6b9b07c
Annylynxs <annakintola7@gmail.com>
2023-04-25 13:51:47 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Learning about nested loop
+ * Return: (0)
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char a;
+
+	n = 0;
+	a = 97;
+	while (n < 10)
+	{
+		while (a < 123)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
+
