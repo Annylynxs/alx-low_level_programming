fbb7d7164aec1138b7797cced89625f523bf1f19
Annylynxs <annakintola7@gmail.com>
2023-04-25 16:15:21 -0700

    absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of a number
+ * @n: input
+ * Return: (0)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (n * -1);
+	}
+	else
+	{
+		n = (n * 1);
+	}
+}
+
